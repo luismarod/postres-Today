2447252fff231e5d5d370f80d8adbb95e63e7cbf
luismarod <luismarod1408@gmail.com>
2020-09-23 17:53:08 -0300

    cargando archivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: MainComponent},
+  {path:'quienes-somos', component: QuienesSomosComponent},
+  {path:'productos', component: ProductosComponent},
+  {path:'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { ProductosComponent } from './productos/productos.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    ProductosComponent,
+    QuienesSomosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
